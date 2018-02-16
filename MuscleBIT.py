db0b271ce9dbd4ee52e49bf0970e6c5552989f11
Valev, Hristo <hristo.valev@philips.com>
2018-02-16 16:25:27 +0100

    update

---
+++ MuscleBIT.py
@@ -0,0 +1,225 @@
+import bitalino
+import numpy as np
+import time
+import random
+import matplotlib.pyplot as plt
+from threading import Thread
+from statsmodels.nonparametric.smoothers_lowess import lowess
+import peakutils.peak
+import math
+
+num_buzz_trig = 20
+treshold = 0
+trialData = []
+trial = True
+
+# Mac OS
+# macAddress = "/dev/tty.BITalino-XX-XX-DevB"
+# Windows
+macAddress = "20:17:09:18:58:60"
+device = bitalino.BITalino(macAddress)
+
+#generate breaks randomly between the different buzzer pulses
+def generatePauses(number):
+    fib = range(number)
+    nums = []
+    for num in fib:
+        #nums.append(random.uniform(2, 5)) -> a little more stressful test!
+        nums.append(random.uniform(3, 6))
+    return nums
+
+
+def dataAcquisition():
+    global trial 
+    global trialData
+    global treshold
+    time.sleep(1)
+    
+    srate = 1000
+    nframes = 100
+
+    device.start(srate, [0])
+    try:
+        while trial:        
+            data = device.read(nframes)            
+            if np.mean(data[:, 1]) < 1: break    
+            EMG = data[:, -1]            
+            envelope = np.mean(abs(np.diff(EMG)))            
+            #disable writing to datastructure if just initialized
+            if(len(trialData) > 0):
+                idx = len(trialData) - 1
+                #check if timestamp has been written -> disallow two peaks within the same trial frame
+                if(trialData[idx][1] == 0 and treshold < envelope):
+                    trialData[idx][1] = time.time()
+    finally:
+        print("STOP")
+        device.stop()
+        device.close()
+
+
+def triggerBuzzer():
+    global trial 
+    global trialData
+    global num_buzz_trig
+    print("The following trial will measure your psychomotoric response.")
+    print("Please activate your muscle as soon as you hear the buzz.")
+    time.sleep(10)
+    print("5")
+    time.sleep(1)
+    print("4")
+    time.sleep(1)
+    print("3")
+    time.sleep(1)
+    print("2")
+    time.sleep(1)
+    print("1")
+    time.sleep(1)
+    print("START")
+    pauses = generatePauses(num_buzz_trig)
+    for pause in pauses:
+        time.sleep(pause)
+        trialData.append([time.time(),0])
+        device.trigger([0, 1])
+        time.sleep(0.2)
+        device.trigger([0, 0])
+    #final pause, to give room for the last assessment before killing both threads
+    time.sleep(5)
+    trial = False
+    pass
+
+def calibrateSensor():
+    global treshold
+    print("In order to calibrate the sensor to the muscle you will be using we require you")
+    print("to activate that muscle three times during an interval of 10 seconds")
+    time.sleep(10)
+    srate = 1000
+    nframes = 100
+    #threshold = 5
+    d = []
+    device.start(srate, [0])
+    try:
+        cal_start = time.time()
+        while(True):
+            now = time.time()
+            print("It has been {0} seconds".format(round(now - cal_start),1))
+            
+            data = device.read(nframes)
+            d.extend(data[:,5])
+            
+            #break loop after 10 seconds
+            if((now - cal_start) > 10):
+                break
+            
+    finally:
+        converted = []
+        for val in d:
+            converted.append(transformData(val))
+        t = range(len(d))
+        filtered = lowess(np.abs(converted), t, is_sorted=True, frac=0.025, it=0)
+        plt.plot(filtered[:,1])
+        plt.show()
+        
+        baseline = np.mean(filtered[:,1])
+        max_val = np.max(filtered[:,1])
+        threshold = (baseline+max_val)/2
+        indexes = peakutils.peak.indexes(np.array(filtered[:,1]),
+                                         threshold, min_dist=50)
+        peaks = []
+        for index in indexes:
+            peaks.append(filtered[:,1][index])
+            
+        print('Peaks are: %s' % (indexes))
+        print('Peak values are: %s' % (peaks))
+        print('Baseline is : %s' % (baseline))
+        print('Treshold is : %s' % (threshold))
+        
+        #we have exactly 3 peaks -> good & don't close socket
+        if(len(peaks) == 3):
+            device.stop()
+            return True   
+    device.stop()
+    device.close()
+    return False
+            
+
+def transformData(ADC):
+    VCC = 3.3
+    G_emg = 1000
+    n = 10
+    return (((ADC/math.pow(2,n)-0.5)*VCC)/G_emg)*1000
+
+def validateResults():
+    global trialData
+    for instance in trialData:
+        if(instance[1] == 0 or instance[0] == 0):
+            return False
+    return True
+
+def drawCharts():
+    global trialData
+    parsed = []
+    for e in trialData:
+        parsed.append(e[1]-e[0])
+    plt.plot(parsed)
+    plt.show()
+    
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    
+    x_points = []
+    means = []
+    std_dev = []
+    mins = []
+    maxes = []
+    upper_boundry = []
+    lower_boundry = []
+    
+    x_points = [0]
+    means.append(np.mean(parsed))
+    std_dev.append(np.std(parsed))
+    mins.append(np.min(parsed))
+    maxes.append(np.max(parsed))
+    upper_boundry.append(means[0] - mins[0])
+    lower_boundry.append(maxes[0] - means[0])
+    
+    ax.plot()
+    ya = ax.get_xaxis()
+    ya.set_ticklabels([])
+    
+    plt.errorbar(x_points, means, [upper_boundry, lower_boundry], fmt='.k', ecolor='blue', lw=5)
+    plt.errorbar(x_points, means, std_dev, fmt='ok', ecolor='red', lw=5)
+    print("The blue line describes the distribution of values obtained from the trial, also depicting the maximum and minimum")
+    print("The red line describes the standard deviation")
+    print("The black dot is the mean of all the values")
+    if(np.mean(parsed) > 0.7):
+        print("You are psychomotorically retarded")
+    pass
+
+
+def main():
+    if(calibrateSensor()):
+        print("Calibration successful")
+        #thread one deals with continuous data acquision
+        t1 = Thread(target = dataAcquisition)
+        #thread two controls the experiment and triggers the buzzer
+        t2 = Thread(target = triggerBuzzer)
+        t1.setDaemon(True)
+        t2.setDaemon(True)
+        t1.start()
+        t2.start()
+        #rejoin threads to execute drawCharts sequentially
+        t1.join()
+        t2.join()
+        if(validateResults()):
+            drawCharts()
+        else:
+            print("Trial unsucessfull, you may have missed to react to the buzzer")
+    else:
+        print("Calibration unsuccessfull, please analyze data")
+    
+
+if __name__ == "__main__":
+    main()
+    
+    
--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # Bitalino-EMG-psychomotoric-test
 Psychomotoric test to measure reaction time using a Bitalino board and EMG as input signal
+
+## Requirements
+python3.5 with packages bitalino, numpy, time, random, matplotlib, threading, statsmodels, peakutils, math and your favorite python IDE
+If running an Anaconda environment, just install external packages like this:
+```
+conda install numpy, matplotlib, statsmodels
+pip install peakutils
+```
+bitalino python api from [here](https://github.com/BITalinoWorld/revolution-python-api).
+
+## Disclaimer
+The code has only been tested for the forearm flexor carpi radialis muscle. Theoretically it should be able to work for any muscle.
+* Sample positioning of the electrodes, where the one on the elbow is the reference electrode.
+![File missing](imgs/Electrode_placement.jpg)
+
+## Trial
+Consist of two phases, calibration and trial, while instructions are given through the console output.
+* In the calibration phase the user is asked to activate their muscle three times in the span of 10 seconds
+* During the trial the user is asked to activate their muscle 20 times (can be configured) at [3,6] seconds intervals
+
+
+### Calibration
+During the calibration phase a 10 seconds fragment of the signal is acquired to estimate, whether there is a significant noise in the signal. For example poorly placed arm will generate slight involuntary movements, which will introduce noise to the signal. Movements of the neighboring muscles or muscle groups will introduce significant noise.
+Ideally, in this phase there will be three distinguishable peaks in the signal, such as this:
+![File missing](plots/calibration.png)
+* The absolute values of the signal is taken as explained [here](https://en.wikipedia.org/wiki/Electromyography#EMG_signal_processing)
+* A smoothing ['lowess' local regression filter](https://en.wikipedia.org/wiki/Local_regression) is applied to smoothen the signal
+* After successful identification of the three peaks in the signal a threshold value is estimated, which is used later for the trial
+```
+baseline = np.mean(filtered[:,1])
+max_val = np.max(filtered[:,1])
+threshold = (baseline+max_val)/2
+```
+The threshold value is calculated to be equidistant from the averaged baseline signal and the highest maximum to allow room for somewhat noisy data.
+* Plot is generated of the signal such as this:
+![File missing](plots/calibration.png
+* Peak indexes, peak values, averaged signal baseline and derived threshold are displayed in the console output.
+
+### Trial
+The following variable can be modified to generate shorter or longer trials. In my experience more than 20 instances already introduces fatigue.
+```
+num_buzz_trig = 20
+```
+* Buzzer will ring for 0.2 milliseconds to provide the stimulus to prompt the muscle to be actuated
+* Peaks are detected in real-time within an envelope frame, by comparing the averaged value to the previously acquired threshold.
+* A plot is created for the time difference between the stimulus and the response, such as those:
+![File missing](plots/plot1)
+![File missing](plots/plot1_1)
+* A plot is created with the maximum and minimum values, standard deviation and mean, such as those:
+![File missing](plots/plot2)
+![File missing](plots/plot2_2)
+The blue line depicts the distribution, with it's extremes on the y-axis denoting the maximum and minimum, while the red line denotes the standard deviation. The black dot is the mean of the obtained values.
